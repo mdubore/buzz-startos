a6fd55f7fd53afeee87b10e993f3e061a69e862a
mdubore <dev@example.com>
2026-07-26 08:13:30 -0700

    chore: scaffold StartOS package

---
+++ startos/i18n/dictionaries/default.ts
@@ -0,0 +1,16 @@
+export const DEFAULT_LANG = 'en_US'
+
+const dict = {
+  // main.ts
+  'Starting Buzz!': 0,
+  'Web Interface': 1,
+  'The web interface is ready': 2,
+  'The web interface is not ready': 3,
+} as const
+
+/**
+ * Plumbing. DO NOT EDIT.
+ */
+export type I18nKey = keyof typeof dict
+export type LangDict = Record<(typeof dict)[I18nKey], string>
+export default dict
---
+++ startos/i18n/dictionaries/translations.ts
@@ -0,0 +1,28 @@
+import { LangDict } from './default'
+
+export default {
+  es_ES: {
+    0: '¡Iniciando Buzz!',
+    1: 'Interfaz web',
+    2: 'La interfaz web está lista',
+    3: 'La interfaz web no está lista',
+  },
+  de_DE: {
+    0: 'Starte Buzz!',
+    1: 'Weboberfläche',
+    2: 'Die Weboberfläche ist bereit',
+    3: 'Die Weboberfläche ist nicht bereit',
+  },
+  pl_PL: {
+    0: 'Uruchamianie Buzz!',
+    1: 'Interfejs webowy',
+    2: 'Interfejs webowy jest gotowy',
+    3: 'Interfejs webowy nie jest gotowy',
+  },
+  fr_FR: {
+    0: 'Démarrage de Buzz !',
+    1: 'Interface web',
+    2: "L'interface web est prête",
+    3: "L'interface web n'est pas prête",
+  },
+} satisfies Record<string, LangDict>
